e0c6ac422a9fe0330136a87885422fd0cdec8cc3
pandeyabhay8 <abhay.pandey@evelynlearning.com>
2023-07-19 13:46:38 +0530

    major-1

---
+++ src/main/java/com/rest/test/Major1Application.java
@@ -0,0 +1,13 @@
+package com.rest.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Major1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Major1Application.class, args);
+	}
+
+}
